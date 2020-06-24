a25acfe4bde790ab66c251ba1ccdd04eba57f5df
Luis Alejandro Pascuaza <lpascuazac@unal.edu.co>
2020-06-23 21:58:13 -0500

    Http get + primera parte visual. Falta hacer el enlace entre componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)
-  },
-  {
-    path: '',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  }
+  { path: '', loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)},
+  { path: '', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'albums', loadChildren: './pages/albums/albums.module#AlbumsPageModule' },
+  { path: 'album/:id', loadChildren: './pages/album/album.module#AlbumPageModule'}, 
 ];
 @NgModule({
   imports: [
