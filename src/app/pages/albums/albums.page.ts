fdc25011c9e02d42a42e79293a212f2ef4cb0f45
Luis Alejandro Pascuaza <lpascuazac@unal.edu.co>
2020-06-25 01:51:54 -0500

    Se añadió el promedio y el consumo. Falta arreglar la parte visual

--- src/app/pages/albums/albums.page.ts
+++ src/app/pages/albums/albums.page.ts
@@ -9,13 +9,51 @@ import { AlbumService } from 'src/app/services/album.service';
 })
 export class AlbumsPage implements OnInit {
 
-  results: Observable<any>;
+  results: Observable<any[]>;
+  public total_avg: number;
+  public consum_avg: number;
+
 
   constructor(private albumService: AlbumService) {
     this.results = this.albumService.getAlbums();
+    this.results.subscribe(elt => {
+    	var results_lenght = elt.length;
+    	console.log(results_lenght);
+
+    	var avg = 0;
+    	for (var i = 0; i < results_lenght; i++) {
+    		avg = avg + elt[i].id;
+    		//console.log(elt[i].id);
+    	}
+    	console.log(avg);
+    	this.total_avg = avg/results_lenght;
+    	console.log("Tot_avg: " + this.total_avg);
+
+    	var kh_p = 450; //Precio kWh
+
+    	this.consum_avg = this.total_avg * kh_p;
+
+    	console.log("Consum_avg: " + this.consum_avg);
+    	});
+    
   }
 
   ngOnInit() {
   }
+
+  
+ 
+/*  this.results_lenght=this.i+1; console.log(this.i);
+	ngAfterContentChecked()  {
+    let avg = 0;
+    let len = 0;
+    for (let i in this.results) {
+    	console.log("Entró")
+    	this.results[i].id;
+     	len = len + 1 ;
+    }
+
+    this.total_avg = len;
+  } */
 
 }
